a414baf42b9a0f26890664fd16e46f38e250007a
helio-l <helio.lancon@ens.uvsq.fr>
2026-03-03 23:15:40 +0100

    Update projet in200.py

--- projet in200.py
+++ projet in200.py
@@ -1,61 +1,45 @@
 from math import sin, cos, radians
 
-
-""" représentation de la grille 
-
-[0, 0, 0, 0, 0]
-[0, 0, 0, 0, 0]
-[0, 0, 0, 0, 0]
-[0, 0, 0, 0, 0]
-[0, 0, 0, 0, 0]
-
-"""
-
-""" valeurs initiales """
-
-
-droite = 0   
-haut = 90            
-gauche = 180    
-bas = 270       
+""" valeurs initiales """ 
 
 event_bouton_play = True
 event_bouton_pause = False
 event_bouton_next = None
 
-
+(droite, haut, gauche, bas) = (0, 90, 180, 270)  
 grille = [5*[0] for _ in range (0, 5)]  #numpy
 case_fourmi = grille[2][2] 
 orientation_fourmi = droite
-i = 2
-j = 2        
+(i, j) = (2, 2)      
 nb_etape = 0
 
 """ début du programme principal """
 
-print(( i-int(sin(orientation_fourmi)) ) % 5, type(( i-int(sin(orientation_fourmi)) ) % 5))
-
 def etape(grille, case_fourmi, orientation_fourmi, i, j, nb_etape):
         orientation_fourmi = ( orientation_fourmi + 270 - 180 * (case_fourmi) ) % 360
         case_fourmi = grille[( i-int(sin(orientation_fourmi)) ) % 5][( j+int(cos(orientation_fourmi)) ) % 5]
-        print(i, j, grille[i][j])
         grille[i][j] += (-1)**grille[i][j]
-        print(case_fourmi)
         nb_etape += 1
-        (i, j) = ((i-int(sin(orientation_fourmi)) ) % 5, (j+int(cos(orientation_fourmi))) % 5)
+        (i, j) = ((i-int(sin(radians(orientation_fourmi))) ) % 5, (j+int(cos(radians(orientation_fourmi)))) % 5)
         return (grille, case_fourmi, orientation_fourmi, i, j, nb_etape)
-
-
-print((grille, case_fourmi, orientation_fourmi, i, j, nb_etape), "initialisation")
-
-for k in range (0,1):
-        (grille, case_fourmi, orientation_fourmi, i, j, nb_etape) = etape(grille, case_fourmi, orientation_fourmi, i, j, nb_etape)
-        print((grille, case_fourmi, orientation_fourmi, i, j, nb_etape)) 
-
-  
-
-
 
 """ if event_bouton_play:
     while not event_bouton_pause:
          etape()    """
+
+
+
+
+""" tests du programme """
+
+print("\n initialisation", (grille, case_fourmi, orientation_fourmi, i, j, nb_etape), "\n")
+
+for k in range (1,10):
+        (grille, case_fourmi, orientation_fourmi, i, j, nb_etape) = etape(grille, case_fourmi, orientation_fourmi, i, j, nb_etape)
+
+        print("matrice etape", k)
+        for p in range (0, 5):
+               print(grille[p])
+
+        print("\n resultat", ("grille:", grille, "couleur case fourmi:", case_fourmi, "orientation fourmi:", orientation_fourmi, 
+                           "ligne:", i+1, "colonne:", j+1, "etape:", nb_etape), "\n ") 
