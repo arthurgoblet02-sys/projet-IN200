74884bfcb00142ea50dd4d54e0d7bc66acb77051
helio-l <helio.lancon@ens.uvsq.fr>
2026-03-03 18:20:00 +0100

    Update projet in200.py

--- projet in200.py
+++ projet in200.py
@@ -6,10 +6,8 @@
 [0, 0, 0, 0, 0]
 [0, 0, 0, 0, 0]
 
-0 0 
-1 0 
-2 1 
-3 1 
+0 r l
+90 u d
 
 """
 
@@ -17,17 +15,19 @@
 """ valeurs initiales """
 
 
-droite = 0
-haut = 90
-gauche = 180
-bas = 270
+droite = 0     
+haut = 90   -1
+gauche = 180    
+bas = 270  1
 
 event_bouton_play = True
 event_bouton_pause = False
 event_bouton_next = None
 
 grille = 5*[5*[0]]  #numpy
-case_fourmi = grille[2][2]                 
+case_fourmi = grille[2][2] 
+i = 2
+j = 2        
 orientation_fourmi = droite
 nb_etape = 0
 
@@ -35,10 +35,32 @@ nb_etape = 0
 
 def etape():
         if case_fourmi == 0:
-            orientation_fourmi = (orientation_fourmi - 90) % 360
-            case_fourmi = None 
+            orientation_fourmi = (orientation_fourmi - 90) % 360                 
+            
+            if orientation_fourmi % 180 == 0:
+                 case_fourmi = grille[i][j - (orientation_fourmi % 90) + 1]
+                 grille[i][j] = 1
+                 j -= (orientation_fourmi % 90) - 1
+            
+            else:
+                 case_fourmi = grille[i - (orientation_fourmi % 135) + 1][j]
+                 grille[i][j] = 1
+                 i -= (orientation_fourmi % 135) - 1    
+        
         if case_fourmi == 1:
             orientation_fourmi = (orientation_fourmi + 90) % 360
+
+            if orientation_fourmi % 180 == 0:
+                 case_fourmi = grille[i][j - (orientation_fourmi % 90) + 1]
+                 grille[i][j] = 0
+                 j -= (orientation_fourmi % 90) - 1
+            
+            else:
+                 case_fourmi = grille[i - (orientation_fourmi % 135) + 1][j]
+                 grille[i][j] += (-1) ** grille[i][j]
+                 i -= (orientation_fourmi % 135) - 1 
+
+
 
 if event_bouton_play:
     while not event_bouton_pause:
