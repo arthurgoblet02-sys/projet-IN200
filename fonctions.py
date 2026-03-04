5d197ae0cc91b0cf533446dcda841259d9997eb1
helio-l <helio.lancon@ens.uvsq.fr>
2026-03-04 18:11:40 +0100

    Update fonctions.py

--- fonctions.py
+++ fonctions.py
@@ -1,1 +1,11 @@
 """ fichier où on écrira les fonctions nécéssaires pour le main """ 
+
+from math import sin, cos, radians
+
+def etape(grille, case_fourmi, orientation_fourmi, i, j, nb_etape):
+        orientation_fourmi = ( orientation_fourmi + 270 - 180 * (case_fourmi) ) % 360
+        case_fourmi = grille[( i-int(sin(orientation_fourmi)) ) % 5][( j+int(cos(orientation_fourmi)) ) % 5]
+        grille[i][j] += (-1)**grille[i][j]
+        nb_etape += 1
+        (i, j) = ((i-int(sin(radians(orientation_fourmi))) ) % 5, (j+int(cos(radians(orientation_fourmi)))) % 5)
+        return (grille, case_fourmi, orientation_fourmi, i, j, nb_etape)
