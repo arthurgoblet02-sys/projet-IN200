757b2af249e9bd3a4d779d1a51666378b8dcc052
arthurgoblet02-sys <arthur.goblet02@gmail.com>
2026-03-06 11:10:38 +0100

    Update plateau.py

--- plateau.py
+++ plateau.py
@@ -1,4 +1,20 @@
 "creer une fonction Plateau pour creer le plateau pour que je puisse le mettre dans interface graphique   "
 from projet_in200 import grille
-from interface_graphique import window
- from tkinter import * 
+from tkinter import *
+
+def Plateau (window):
+    nbr_colonne = len(grille)
+    nbr_ligne = len(grille[0])
+    taille_case = 10
+    colonne = nbr_colonne * taille_case
+    ligne = nbr_ligne * taille_case
+    canva = Canvas (window,width=colonne,height=ligne, bg="white" )
+    canva.grid(row=1,column= 1 )
+    for i in range(nbr_colonne):
+        for j in range(nbr_ligne):
+            if grille[i][j] == 1:
+                couleur = "black"
+            else:
+                couleur = "white"
+            canva.create_rectangle (i * taille_case, j*taille_case,fill=couleur outline= "green" ) 
+    return canva 
