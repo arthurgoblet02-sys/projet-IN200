9db8f94c81fe8ded0948f3e22ec539232f503f51
arthurgoblet02-sys <arthur.goblet02@gmail.com>
2026-03-06 10:42:09 +0100

    Update plateau.py

--- plateau.py
+++ plateau.py
@@ -1,8 +1,4 @@
 "creer une fonction Plateau pour creer le plateau pour que je puisse le mettre dans interface graphique   "
-from projet_in200.py import grille 
-def Plateau (grille):
-
-
-
-
-    return plateau 
+from projet_in200 import grille
+from interface_graphique import window
+ from tkinter import * 
