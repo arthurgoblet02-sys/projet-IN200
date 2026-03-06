5b79916dca934fdb65a5fbcde0a38d6b2d069441
arthurgoblet02-sys <arthur.goblet02@gmail.com>
2026-03-06 11:11:27 +0100

    Update interface_graphique .py

--- interface_graphique .py
+++ interface_graphique .py
@@ -3,7 +3,7 @@
 
 
 from tkinter import *
-#from plateau import *
+from plateau import *
 
 window = Tk()
 window.title("Jeu de la fourmi de Langton")
@@ -14,8 +14,11 @@ arriere_plan = Label(window ,image =fond)
 arriere_plan.grid(row=0, column=0, rowspan=3, columnspan=3)
 window.grid_rowconfigure(1, weight=1)
 window.grid_columnconfigure(1,weight=1)
+plateau_jeu = Plateau (window) 
 def on_button_click():
     bouton_jouer.destroy()
+    return plateau_jeu
+    
 
 bouton_jouer = Button (window , text="JOUER", bg="green", command=on_button_click)
 bouton_jouer.grid(row=1 ,column=1)
