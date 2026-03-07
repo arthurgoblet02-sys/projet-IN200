eb661641bc8b46f7b6522e31ff976e7d730d971f
arthurgoblet02-sys <arthur.goblet02@gmail.com>
2026-03-07 08:59:41 +0100

    Update bouton.py

--- bouton.py
+++ bouton.py
@@ -1,1 +1,4 @@
-
+" il faut creer tout les bouton demander dans le sujet donc le bouton play, pause et etapes. lire le sujet pour savoir ce que font ces bouton."
+Play = " creer le bouton il faut qu'il s'appelle comme ca car il est appeler comme ca dans interface graphique il ce place a gauche donc mettre row=1 ,column=0 quand vou le placer   "
+Pause = " creer le bouton il faut qu'il s'appelle comme ca car il est appeler comme ca dans interface graphique lui c'est row=0 ,column=1"
+Next = " creer le bouton il faut qu'il s'appelle comme ca car il est appeler comme ca dans interface graphique et lui row=1 ,column=2"
