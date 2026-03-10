8aa8ef6e2c65f821aa2b9844ee50bc38920dd07b
arthurgoblet02-sys <arthur.goblet02@gmail.com>
2026-03-10 08:52:11 +0100

    Update bouton.py

--- bouton.py
+++ bouton.py
@@ -1,5 +1,5 @@
 " il faut creer tout les bouton demander dans le sujet donc le bouton play, pause et etapes. lire le sujet pour savoir ce que font ces bouton."
-"les fonction que vous aller mettre dans comand a la creation du bouton faut les rajouter dans le fichier fonction "
+"les fonction que vous aller mettre dans command a la creation du bouton faut les rajouter dans le fichier fonction "
 
 from fonctions import *
 
