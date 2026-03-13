f36daca81d6d2a3af683d9489f4b0738f057df80
helio-l <helio.lancon@ens.uvsq.fr>
2026-03-13 23:34:10 +0100

    Create interface_graphique .py

---
+++ interface_graphique .py
@@ -0,0 +1,30 @@
+" creation du fond du jeu avec le bouton jouer au milieu de l'ecran."
+"IL MANQUE JUSTE DE RAJOUTER LE PLATEAU QUAND ON CLIQUE SUR LE BOUTON A LA LIGNE 17 "
+
+
+from tkinter import * 
+from plateau import *
+from bouton import *
+
+window = Tk()
+window.title("Jeu de la fourmi de Langton")
+window.geometry("850x600")
+
+fond = PhotoImage(file = "fond.png")
+arriere_plan = Label(window ,image =fond) 
+arriere_plan.grid(row=0, column=0, rowspan=3, columnspan=3)
+window.grid_rowconfigure(1, weight=1)
+window.grid_columnconfigure(1,weight=1)
+
+
+
+def on_button_click():
+    bouton_jouer.destroy()
+    return Plateau (window), bouton_Play, bouton_Pause, bouton_Next
+    
+
+bouton_jouer = Button (window , text="PLAY", bg="green",fg="white", command=on_button_click)
+bouton_jouer.grid(row=1 ,column=1,ipady=10,ipadx=10)
+
+window.mainloop()
+
