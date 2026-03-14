ef393a48b36f2baf44a027d999474d8d324941bd
helio-l <helio.lancon@ens.uvsq.fr>
2026-03-14 01:28:58 +0100

    Update fonctions.py

--- fonctions.py
+++ fonctions.py
@@ -4,10 +4,18 @@
 
 from math import sin, cos, radians
 
-def etape(grille, case_fourmi, orientation_fourmi, i, j, nb_etape, side):
+def step(grille, case_fourmi, orientation_fourmi, i, j, nb_etape, side):
         orientation_fourmi = ( orientation_fourmi + 270 - 180 * (case_fourmi) ) % 360
         case_fourmi = grille[( i-int(sin(orientation_fourmi)) ) % side][( j+int(cos(orientation_fourmi)) ) % side]
         grille[i][j] += (-1)**grille[i][j]
         nb_etape += 1
         (i, j) = ((i-int(sin(radians(orientation_fourmi))) ) % side, (j+int(cos(radians(orientation_fourmi)))) % side)
+        return (grille, case_fourmi, orientation_fourmi, i, j, nb_etape, side)
+
+def back_step(grille, case_fourmi, orientation_fourmi, i, j, nb_etape, side):
+        (i, j) = ((i+int(sin(radians(orientation_fourmi))) ) % side, (j-int(cos(radians(orientation_fourmi)))) % side)
+        nb_etape -= 1
+        grille[i][j] += (-1)**grille[i][j]
+        case_fourmi = grille[( i+int(sin(orientation_fourmi)) ) % side][( j-int(cos(orientation_fourmi)) ) % side]
+        orientation_fourmi = ( orientation_fourmi - 270 + 180 * (case_fourmi) ) % 360
         return (grille, case_fourmi, orientation_fourmi, i, j, nb_etape, side)
