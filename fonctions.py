c723170504e56c4674443ccc23292d99b7d787fb
helio-l <helio.lancon@ens.uvsq.fr>
2026-03-14 23:29:57 +0100

    Update fonctions.py

--- fonctions.py
+++ fonctions.py
@@ -8,17 +8,17 @@ def step(grille, case_fourmi, orientation_fourmi, i, j, nb_etape, side):
         orientation_fourmi = ( orientation_fourmi + 270 - 180 * (case_fourmi) ) % 360
         case_fourmi = grille[(i-int(sin(radians(orientation_fourmi)))) % side][(j+int(cos(radians(orientation_fourmi)))) % side]
         grille[i][j] += (-1)**grille[i][j]
-        nb_etape += 1
         (i, j) = ((i-int(sin(radians(orientation_fourmi))) ) % side, (j+int(cos(radians(orientation_fourmi)))) % side)
+        nb_etape += 1
         return (grille, case_fourmi, orientation_fourmi, i, j, nb_etape, side)
 
 
 def back_step(grille, case_fourmi, orientation_fourmi, i, j, nb_etape, side):
-        (i, j) = ((i+int(sin(radians(orientation_fourmi))) ) % side, (j-int(cos(radians(orientation_fourmi)))) % side)      
-        nb_etape -= 1
+        (i, j) = ((i+int(sin(radians(orientation_fourmi))) ) % side, (j-int(cos(radians(orientation_fourmi)))) % side) 
         grille[i][j] += (-1)**grille[i][j]
         case_fourmi = grille[i][j]
         orientation_fourmi = ( orientation_fourmi - 270 + 180 * (case_fourmi) ) % 360
+        nb_etape -= 1
         return (grille, case_fourmi, orientation_fourmi, i, j, nb_etape, side)
 
 
