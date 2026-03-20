abdf6de4aa5c2b3b11f6330073fdff101e5264ca
Seydou92 <seydou.sylla030@gmail.com>
2026-03-20 08:10:22 +0100

    Update fourmi.py

--- fourmi.py
+++ fourmi.py
@@ -1,1 +1,6 @@
-
+""" voici l'algo pour dessiner la fourmi
+    Rôle de la fonction :
+    1. Efface l'ancien dessin de la fourmi sur le plateau pour ne pas laisser de traces.
+    2. Calcule les coordonnées des 3 pointes d'un triangle en fonction de la case actuelle (i, j) et de la direction regardée (haut, bas, gauche, droite).
+    3. Dessine la nouvelle fourmi (le triangle rouge) sur le canevas.
+    """
