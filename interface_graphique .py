3e8b995a4f6c7ba966e8fcbfe3bd828569666518
arthurgoblet02-sys <arthur.goblet02@gmail.com>
2026-03-20 09:39:53 +0100

    Update interface_graphique .py

--- interface_graphique .py
+++ interface_graphique .py
@@ -6,28 +6,28 @@ from tkinter import *
 from plateau import *
 from bouton import *
 
-window = Tk()
-window.title("Jeu de la fourmi de Langton")
-window.geometry("850x600")
+fenetre = Tk()
+fenetre.title("Jeu de la fourmi de Langton")
+fenetre.geometry("850x600")
 
-fond = PhotoImage(file = "fond.png",master=window)
-arriere_plan = Label(window ,image =fond) 
+fond = PhotoImage(file = "fond.png",master=fenetre)
+arriere_plan = Label(fenetre ,image =fond) 
 arriere_plan.grid(row=0, column=0, rowspan=3, columnspan=3)
-window.grid_rowconfigure(1, weight=1)
-window.grid_columnconfigure(1,weight=1)
+fenetre.grid_rowconfigure(1, weight=1)
+fenetre.grid_columnconfigure(1,weight=1)
 
 
 def button_jouer():
     bouton_play.destroy()
     # bouton_stop.grid(row=2, column=1, ipady=5, ipadx=5)
-    return Plateau (window), bouton_Play, bouton_Pause, bouton_Next,#bouton_stop 
+    return Plateau (fenetre), bouton_Play, bouton_Pause, bouton_Next,#bouton_stop 
     
 
-bouton_play = Button (window , text="PLAY", bg="green",fg="white", command=button_jouer)
+bouton_play = Button (fenetre , text="PLAY", bg="green",fg="white", command=button_jouer)
 bouton_play.grid(row=1 ,column=1,ipady=10,ipadx=10)
 # bouton_stop = Button(window, text="QUITTER & SAUVER", bg="red", fg="white", command=quitter)
 
-window.mainloop()
+fenetre.mainloop()
 
 "il faudra qu'on rajoute la fourmi"
 
