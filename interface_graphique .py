e11e04ef9a665e707142d3ae1614c0e384dfcd7d
helio-l <helio.lancon@ens.uvsq.fr>
2026-04-03 17:56:13 +0200

    Update interface_graphique .py

--- interface_graphique .py
+++ interface_graphique .py
@@ -10,21 +10,22 @@ window = Tk()
 window.title("Jeu de la fourmi de Langton")
 window.geometry("850x600")
 
-fond = PhotoImage(file = "fond.png")
+fond = PhotoImage(file = "fond.png",master=window)
 arriere_plan = Label(window ,image =fond) 
 arriere_plan.grid(row=0, column=0, rowspan=3, columnspan=3)
 window.grid_rowconfigure(1, weight=1)
 window.grid_columnconfigure(1,weight=1)
 
 
-
-def on_button_click():
+def button_jouer():
     bouton_play.destroy()
-    return Plateau (window), bouton_Play, bouton_Pause, bouton_Next
+    # bouton_stop.grid(row=2, column=1, ipady=5, ipadx=5)
+    return Plateau (window), bouton_Play, bouton_Pause, bouton_Next,#bouton_stop 
     
 
-bouton_play = Button (window , text="PLAY", bg="green",fg="white", command=on_button_click)
+bouton_play = Button (window , text="PLAY", bg="green",fg="white", command=button_jouer)
 bouton_play.grid(row=1 ,column=1,ipady=10,ipadx=10)
+# bouton_stop = Button(window, text="QUITTER & SAUVER", bg="red", fg="white", command=quitter)
 
 window.mainloop()
 
