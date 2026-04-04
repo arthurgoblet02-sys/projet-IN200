74aa7c8bc947f81135ced5a2fd1337f3cbfdcd9c
helio-l <helio.lancon@ens.uvsq.fr>
2026-04-04 21:41:01 +0200

    changes

--- fonctions.py
+++ fonctions.py
@@ -1,26 +1,26 @@
-""" fichier où on écrira les fonctions nécéssaires pour le main """
-" rajouter une fonction vitesse pour augmenter la vitesse d'execution des etape quand on clique sur play "
-"rajouter une fonction permetant de revenir en arriere d'une etape " 
-
 from math import sin, cos, radians
 #from projet_in200 import *
 
-def step(grille, etat_case_fourmi, orientation_fourmi, i_j, nb_etape, side):
+def step(grille, etat_case_fourmi, orientation_fourmi, i_j, nb_etape, side, nb_fourmis, modulo):
+        etat_case_fourmi = grille[i_j[0]][i_j[1]]
         orientation_fourmi = ( orientation_fourmi + 270 - 180 * (etat_case_fourmi) ) % 360
         etat_case_fourmi = grille[(i_j[0] - int(sin(radians(orientation_fourmi)))) % side][(i_j[1] + int(cos(radians(orientation_fourmi)))) % side]
         grille[i_j[0]][i_j[1]] += (-1)**grille[i_j[0]][i_j[1]]
         (i_j[0], i_j[1]) = ((i_j[0] - int(sin(radians(orientation_fourmi))) ) % side, (i_j[1] + int(cos(radians(orientation_fourmi)))) % side)
         nb_etape += 1
-        return (grille, etat_case_fourmi, orientation_fourmi, i_j, nb_etape, side)
+        modulo = (modulo + 1) % nb_fourmis
+        
+        return (grille, etat_case_fourmi, orientation_fourmi, i_j, nb_etape, side, nb_fourmis, modulo)
 
 
-def back_step(grille, etat_case_fourmi, orientation_fourmi, i_j, nb_etape, side):
+def back_step(grille, etat_case_fourmi, orientation_fourmi, i_j, nb_etape, side, nb_fourmis, modulo):
         (i_j[0], i_j[1]) = ((i_j[0] + int(sin(radians(orientation_fourmi))) ) % side, (i_j[1] - int(cos(radians(orientation_fourmi)))) % side) 
         grille[i_j[0]][i_j[1]] += (-1)**grille[i_j[0]][i_j[1]]
         etat_case_fourmi = grille[i_j[0]][i_j[1]]
         orientation_fourmi = ( orientation_fourmi - 270 + 180 * (etat_case_fourmi) ) % 360
         nb_etape -= 1
-        return (grille, etat_case_fourmi, orientation_fourmi, i_j, nb_etape, side)
+        modulo = (modulo - 1) % nb_fourmis
+        return (grille, etat_case_fourmi, orientation_fourmi, i_j, nb_etape, side, nb_fourmis, modulo)
 
  
 
--- projet_in200.py
+++ projet_in200.py
@@ -1,5 +1,6 @@
 from fonctions import step, back_step
 from time import sleep
+from random import randint
 # from sauvegarde import charger # <-- Mis en COM (Désactivés car j'ai pas encore finit )
 
 """ valeurs initiales """ 
@@ -10,28 +11,32 @@ from time import sleep
 #     (grille, case_fourmi, orientation_fourmi, i, j, nb_etape, side, speed) = donnees
 # else:
 
-nb_fourmis = 2
-liste_fourmis = ["fourmi" + str(i) for i in range(nb_fourmis)]
+#mettre des numpy.array() partout au lieu des lists
 
+#valeurs initiales à déterminer
+nb_fourmis = 2
 side = 5
-grille = [side*[0] for _ in range (side)]  #numpy.array()
+#liste_case_fourmi = [[side//2, side//2] for _ in range(nb_fourmis)]
+liste_case_fourmi = [[randint(0,side-1), randint(0,side-1)] for _ in range(nb_fourmis)]
+
+#valeurs initiales fixées
+liste_fourmis = ["fourmi" + str(i) for i in range(nb_fourmis)]
+grille = [side*[0] for _ in range (side)] 
 (droite, haut, gauche, bas) = (0, 90, 180, 270) 
 nb_etape = 0
-modulo = nb_etape % nb_fourmis
-speed = 1.5
-
+modulo = 0
+speed = 10
 liste_orientation_fourmi = nb_fourmis * [droite]
-liste_case_fourmi = [[side//2, side//2] for _ in range(nb_fourmis)]
 liste_etat_case_fourmi = nb_fourmis * [0]
 
 """ tests du programme """
 
-t = 2
+steps = 10
 
-for p in range(t):
-      (grille, liste_etat_case_fourmi[modulo], liste_orientation_fourmi[modulo], liste_case_fourmi[modulo], nb_etape, side) = step(grille, liste_etat_case_fourmi[modulo], liste_orientation_fourmi[modulo], liste_case_fourmi[modulo], nb_etape, side)
+for k in range(steps):
+      (grille, liste_etat_case_fourmi[modulo], liste_orientation_fourmi[modulo], liste_case_fourmi[modulo], nb_etape, side, nb_fourmis, modulo) = step(grille, liste_etat_case_fourmi[modulo], liste_orientation_fourmi[modulo], liste_case_fourmi[modulo], nb_etape, side, nb_fourmis, modulo)
       print(liste_etat_case_fourmi, liste_orientation_fourmi, liste_case_fourmi)
       sleep(1 / speed)
-      print("\n", "step", p+1, "\n")
+      print("\n", "step", k+1, "\n")
       for k in grille:
             print(k) 
