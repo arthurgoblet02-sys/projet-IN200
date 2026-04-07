64846a44c8025634dc7f5cc632c4cffc1363023b
arthurgoblet02-sys <arthur.goblet02@gmail.com>
2026-04-07 09:19:21 +0200

    Update bouton.py

--- bouton.py
+++ bouton.py
@@ -3,12 +3,12 @@
 
 from fonctions import *
 from tkinter import *
-bouton_Play = Button(text = "Play",command=fonction_play)
+bouton_Play = Button(window, text = "Play",command=fonction_play)
 bouton_Play.grid(row = 0,column=0,padx=20,pady=20)
 " creer le bouton, il faut qu'il s'appelle comme ca car il est appeler comme ca dans interface graphique il ce place a gauche donc mettre row=0 ,column=0,padx=20,pady=20 quand vous le placer   "
-bouton_Pause = Button(text="Pause",command=fonction_pause)
+bouton_Pause = Button(window, text="Pause",command=fonction_pause)
 bouton_Pause.grid( row=1,column=0,padx=20)
 " creer le bouton, il faut qu'il s'appelle comme ca car il est appeler comme ca dans interface graphique lui c'est row=1 ,column=0,padx=20"
-bouton_Next = Button(text="Next",command=step)
+bouton_Next = Button(window ,text="Next",command=step)
 bouton_Next.grid(row=2, column=0, padx=20,pady=20)
 
