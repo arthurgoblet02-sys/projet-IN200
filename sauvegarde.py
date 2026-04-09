67721242e1536808359719c11faf108f67076d8a
Seydou92 <seydou.sylla030@gmail.com>
2026-04-09 18:05:03 +0200

    Update sauvegarde.py

--- sauvegarde.py
+++ sauvegarde.py
@@ -1,6 +1,5 @@
 import json 
 
-def sauvegarde(grille, case_fourmi, orientation_fourmi, i, j, nb_etape, side, speed):
-    # 1. On stocke toutes tes variables dans un dictionnaire
+def sauvegarde(grille, case_fourmi, orientation_fourmi, i, j, nb_etape, coté, vitesse):
     donnees = {"case_fourmi": case_fourmi,"orientation_fourmi": orientation_fourmi,"i": i,
-        "j": j,"nb_etape": nb_etape,"side": side,"speed": speed,"grille": grille}
+        "j": j,"nb_etape": nb_etape,"side": coté,"speed": vitesse,"grille": grille}
