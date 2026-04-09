9a0d1cd1c5a215aee9702ae855ef0a2121a246f9
Seydou92 <seydou.sylla030@gmail.com>
2026-04-09 17:28:54 +0200

    mise a jour

--- sauvegarde.py
+++ sauvegarde.py
@@ -1,44 +1,6 @@
-import os 
+import json 
 
 def sauvegarde(grille, case_fourmi, orientation_fourmi, i, j, nb_etape, side, speed):
-    fichier = open("sauvegarde.txt", "w")
-    parametres_jeu = str(case_fourmi) + " " + str(orientation_fourmi) + " " + str(i) + " " + str(j) + " " + str(nb_etape) + " " + str(side) + " " + str(speed) + "\n"
-    fichier.write(parametres_jeu)
-    
-    for ligne in grille:
-        txt = ""
-        for case in ligne:
-            txt = txt + str(case) + " "
-        fichier.write(txt + "\n")
-    fichier.close() 
-
-def charger():
-    if os.path.exists("sauvegarde.txt"):
-        fichier = open("sauvegarde.txt", "r") 
-        ligne_variables = fichier.readline()
-        mots = ligne_variables.split() 
-            
-        case_fourmi = int(mots[0])
-        orientation_fourmi = int(mots[1])
-        i = int(mots[2])
-        j = int(mots[3])
-        nb_etape = int(mots[4])
-        side = int(mots[5])
-        speed = int(mots[6])
-        grille = []
-        for ligne in fichier:
-            ligne_chiffres = []
-            mots_ligne = ligne.split() 
-            for case in mots_ligne:
-                ligne_chiffres.append(int(case))
-            if len(ligne_chiffres) > 0:
-                grille.append(ligne_chiffres)
-                    
-        fichier.close()
-        return grille, case_fourmi, orientation_fourmi, i, j, nb_etape, side, speed
-        
-    else:
-        return None
-    
-    #pas encore trouvé de test. 
-    #aide https://docs.python.org/fr/3/, https://www.w3schools.com/python/,https://www.geeksforgeeks.org/python-programming-language/
+    # 1. On stocke toutes tes variables dans un dictionnaire
+    donnees = {"case_fourmi": case_fourmi,"orientation_fourmi": orientation_fourmi,"i": i,
+        "j": j,"nb_etape": nb_etape,"side": side,"speed": speed,"grille": grille}
