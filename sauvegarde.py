626ece1a35210bc0e1aaff5330d3a82ab56c8e36
Seydou92 <seydou.sylla030@gmail.com>
2026-04-09 18:06:32 +0200

    Update sauvegarde.py

--- sauvegarde.py
+++ sauvegarde.py
@@ -2,4 +2,4 @@ import json
 
 def sauvegarde(grille, case_fourmi, orientation_fourmi, i, j, nb_etape, coté, vitesse):
     donnees = {"case_fourmi": case_fourmi,"orientation_fourmi": orientation_fourmi,"i": i,
-        "j": j,"nb_etape": nb_etape,"side": coté,"speed": vitesse,"grille": grille}
+        "j": j,"nb_etape": nb_etape,"coté": coté,"vitesse": vitesse,"grille": grille}
