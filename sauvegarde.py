5140d478246cc6d3d8d01f7fc62944271896210d
Seydou92 <seydou.sylla030@gmail.com>
2026-04-09 22:18:53 +0200

    Update sauvegarde.py

--- sauvegarde.py
+++ sauvegarde.py
@@ -3,3 +3,6 @@ import json
 def sauvegarde(grille, case_fourmi, orientation_fourmi, i, j, nb_etape, coté, vitesse):
     donnees = {"case_fourmi": case_fourmi,"orientation_fourmi": orientation_fourmi,"i": i,
         "j": j,"nb_etape": nb_etape,"coté": coté,"vitesse": vitesse,"grille": grille}
+    fichier = open("sauvegarde.json", "w")
+    json.dump(donnees, fichier)
+    fichier.close()
