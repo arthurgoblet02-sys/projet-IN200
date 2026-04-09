0547f75e507e38067f4ab855484077fe108bc177
Seydou92 <seydou.sylla030@gmail.com>
2026-04-09 23:24:53 +0200

    Update sauvegarde.py

--- sauvegarde.py
+++ sauvegarde.py
@@ -1,8 +1,18 @@
 import json 
 
-def sauvegarde(grille, case_fourmi, orientation_fourmi, i, j, nb_etape, coté, vitesse):
-    donnees = {"case_fourmi": case_fourmi,"orientation_fourmi": orientation_fourmi,"i": i,
-        "j": j,"nb_etape": nb_etape,"coté": coté,"vitesse": vitesse,"grille": grille}
+def fonction_sauvegarde():
+    # On va chercher les variables globales directement dans le programme
+    global grille, liste_case_fourmi, liste_orientation_fourmi, nb_etape, side, speed, nb_fourmis, liste_etat_case_fourmi
+    
+    # On stocke toutes les variables actuelles du jeu
+    donnees = {"liste_case_fourmi": liste_case_fourmi,"liste_orientation_fourmi": liste_orientation_fourmi,"nb_etape": nb_etape,
+        "side": side,"speed": speed,"nb_fourmis": nb_fourmis,"liste_etat_case_fourmi": liste_etat_case_fourmi,"grille": grille}
+    
+    # 1. On ouvre en mode écriture
     fichier = open("sauvegarde.json", "w")
+    # 2. On écrit dans le fichier
     json.dump(donnees, fichier)
+    # 3. On ferme le fichier
     fichier.close()
+    print("Partie sauvegardée avec succès !")
+
