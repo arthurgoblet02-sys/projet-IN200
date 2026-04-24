48d702760f4afe7ac971d96ef252e9150ac6546d
helio-l <helio.lancon@ens.uvsq.fr>
2026-04-24 09:41:33 +0200

    Update interface_graphique .py

--- interface_graphique .py
+++ interface_graphique .py
@@ -201,70 +201,11 @@ bouton_plus_0_25.grid(row=0, column=5)
 bouton_plus_1.grid(row=0, column=6)
 bouton_plus_10.grid(row=0, column=7)
 
-"""tests pour les settings"""
-
-"""def on_central_square_click():
-     global liste_case_fourmi
-     on_button_reset()
-     mettre_pause()
-     for k in range(nb_fourmis):
-        liste_case_fourmi[k] = [side//2, side//2]
-
-def on_random_squares_click():
-     global liste_case_fourmi
-     on_button_reset()
-     mettre_pause()
-     for k in range(nb_fourmis):
-        liste_case_fourmi[k] = [randint(0,side-1), randint(0,side-1)]
-
-def on_go_to_step_click():
-     global nb_etape 
-     mettre_pause()
-     entry = Entry()
-     entry.grid(row=0, column=1)
-     n = int(input("go to step"))
-     while n < nb_etape:
-          next_()
-     while n > nb_etape:
-          back_()
-     actualiser_affichage()
-
-def on_enter_speed_click():
-     global speed 
-     
-def on_ant_number_click():
-     global nb_fourmis, liste_case_fourmi, liste_etat_case_fourmi, liste_orientation_fourmi
-     
-def on_grid_length_click():
-     global side, grille
 
 menubar = Menu(window)
 menu_settings  = Menu(menubar, tearoff=0)
 menubar.add_cascade(label="settings", menu=menu_settings)
-
-menu_settings.add_command(label="initial values")
-
-menu_settings.add_separator()
-
-menu_settings.add_command(label="ants number", command=None)
-menu_settings.add_command(label="grid length", command=None)
-
-menu_cases = Menu(menu_settings, tearoff=0)
-menu_settings.add_cascade(label="initial squares", menu=menu_cases)
-menu_cases.add_command(label="central square", command=on_central_square_click)
-menu_cases.add_command(label="random squares", command=on_random_squares_click)
-
-
-menu_settings.add_separator()
-
-menu_settings.add_command(label="other settings")
-
-menu_settings.add_separator()
-
-menu_settings.add_command(label="go to step", command=on_go_to_step_click)
-menu_settings.add_command(label="enter speed", command=None)
-
-window.config(menu=menubar)"""
+window.config(menu=menubar)
 
 
 
