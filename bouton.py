c5f02becb54020ecb435ffdf2b240015abc91d7f
arthurgoblet02-sys <arthur.goblet02@gmail.com>
2026-04-25 11:50:50 +0200

    Delete bouton.py

--- bouton.py
+++
@@ -1,8 +0,0 @@
-" il faut creer tout les bouton demander dans le sujet donc le bouton play, pause et etapes. lire le sujet pour savoir ce que font ces bouton."
-"les fonction que vous aller mettre dans command a la creation du bouton faut les rajouter dans le fichier fonction "
-
-from fonctions import *
-from tkinter import *
-
-#bouton_Sauvegarde = Button(window, text="Sauvegarder", command=fonction_sauvegarde)#
-#bouton_Charger = Button(window, text="Charger", command=fonction_charger)# "creer la fonction charger qui va permettre de charger une partie sauvegarder et de continuer a jouer a partir de la partie charger"
